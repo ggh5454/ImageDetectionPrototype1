0dd4befe91ebbf520e8282aafc42574e017a3625
ggh5454 <60964196+ggh5454@users.noreply.github.com>
2022-01-28 18:03:01 +0900

    feat: 갤러리 기능, 이미지 뷰에 사진 올리는 기능 추가

--- app/src/main/java/com/iunga/imagedetectionprototype1/MainActivity.kt
+++ app/src/main/java/com/iunga/imagedetectionprototype1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.iunga.imagedetectionprototype1
 
 import android.R.attr
+import android.app.Activity
 import android.content.Intent
+import android.content.Intent.ACTION_PICK
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -18,11 +20,15 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.drawToBitmap
+import androidx.core.view.isVisible
+import java.net.URI
 
 
 class MainActivity : AppCompatActivity() {
-
-
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_GALLERY = 2
+    }
 
     private val cameraButton: Button by lazy {
         findViewById(R.id.cameraButton)
@@ -30,13 +36,39 @@ class MainActivity : AppCompatActivity() {
     private val imageView: ImageView by lazy {
         findViewById(R.id.imageView)
     }
+    private val galleryButton: Button by lazy {
+        findViewById(R.id.galleryButton)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initCameraButton()
+        initGalleryButton()
+    }
 
+    private fun initGalleryButton() {
+        galleryButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    navigateGallery()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    showPermissionContextPopup(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        500
+                    )
+                }
+            }
+        }
     }
 
     private fun initCameraButton() {
@@ -49,8 +81,9 @@ class MainActivity : AppCompatActivity() {
                     navigateCamera()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
-                    showPermissionContextPopup()
+                    showPermissionContextPopup(android.Manifest.permission.CAMERA)
                 }
+
                 else -> {
                     requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1000)
                 }
@@ -58,35 +91,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY)
+    }
+
 
     private fun navigateCamera() {
-        //TODO 촬영 후 이미지 뷰에 이미지
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-            }
-        }
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
     }
-    val REQUEST_IMAGE_CAPTURE = 1
 
-    //TODO 이미지 생성 URL 변수에 넣고 이 URL 변수로
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.getParcelableExtra<Bitmap>("data")
-            Log.d("imageUri:  ", imageBitmap.toString())
-            imageView.setImageBitmap(imageBitmap)
+        if (resultCode != Activity.RESULT_OK) {
+            return
         }
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                val imageBitmap = data?.getParcelableExtra<Bitmap>("data")
+                imageView.visibility = View.VISIBLE
+                imageView.setImageBitmap(imageBitmap)
+            }
+            REQUEST_GALLERY -> {
+                val imageUri: Uri? = data?.data
+
+                if (imageUri != null) {
+                    imageView.visibility = View.VISIBLE
+                    imageView.setImageURI(imageUri)
+
+                }
+            }
+        }
+
     }
 
-    //TODO 갤러리 권한, 쓰기/읽기 권한 있을 시 에러가 날 수 있음
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.d("RequestPermissionResult(): ", "${requestCode}, ${permissions.contentToString()}, ${grantResults.contentToString()}")
         when (requestCode) {
             1000 -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -95,16 +142,37 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "권한을 거부하셔씁니다.", Toast.LENGTH_SHORT).show()
                 }
             }
+            500 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    navigateGallery()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셔씁니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
-    private fun showPermissionContextPopup() {
-        AlertDialog.Builder(this).setTitle("카메라 권한이 필요합니다.").setMessage("카메라 촬영을 위해서 필요합니다.")
-            .setPositiveButton("동의하기") { _, _ ->
-                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1000)
+    private fun showPermissionContextPopup(permission: String) {
+        when (permission) {
+            android.Manifest.permission.CAMERA -> {
+                AlertDialog.Builder(this).setTitle("카메라 권한이 필요합니다.")
+                    .setMessage("카메라 촬영을 위해서 필요합니다.")
+                    .setPositiveButton("동의하기") { _, _ ->
+                        requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1000)
+                    }
+                    .setNegativeButton("취소하기") { _, _ -> }
+                    .create().show()
             }
-            .setNegativeButton("취소하기") { _, _ -> }
-            .create().show()
+            android.Manifest.permission.READ_EXTERNAL_STORAGE -> {
+                AlertDialog.Builder(this).setTitle("갤러리 권한이 필요합니다.")
+                    .setMessage("갤러리 사진 선택을 위해서 필요합니다.")
+                    .setPositiveButton("동의하기") { _, _ ->
+                        requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1000)
+                    }
+                    .setNegativeButton("취소하기") { _, _ -> }
+                    .create().show()
+            }
+        }
     }
 
 
